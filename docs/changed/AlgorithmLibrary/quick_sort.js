a81dae8e530920c10bb9fb77df6ce6ce04826ed4
evenhumble <patrickwuke@163.com>
2017-10-16 16:36:32 +0800

    remove all

--- docs/changed/AlgorithmLibrary/quick_sort.js
+++
@@ -1,3 +0,0 @@
-function quickSort(aw) {
-    this.init(aw);
-}
--- leet_lint_code/strings/longest_word.py
+++
@@ -1,6 +0,0 @@
-# -*- coding:utf-8 -*-
-class Solution:
-    # @param dictionary: a list of strings
-    # @return: a list of strings
-    def longestWords(self, dictionary):
-        """for loop"""
