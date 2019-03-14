b955c36c92f51b7ed29b7dab661920daaf137260
evenhumble <patrickwuke@163.com>
2019-03-15 06:31:02 +0800

    add tree related

---
+++ algorithm_python/leet_lint_code/tree/topic_tree.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+-------------------------------------------------
+   File Name：     topic_tree
+   Description :   https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/992/
+   Author :        patrick
+   date：          2019/3/15
+-------------------------------------------------
+   Change Activity:
+                   2019/3/15:
+-------------------------------------------------
+"""
+__author__ = 'patrick'
+
+# root, then left , then right
+def per_order_tree_traveral(node):
+    """
+    root,left first, then right
+    :param node:
+    :return:
+    """
+    pass
+
+# traverse left first, then root ,then right
+def in_order_tree_traveral(node):
+    pass
+
+# left,right,finally root
+def post_order_tree_traveral(node):
+    pass
+
