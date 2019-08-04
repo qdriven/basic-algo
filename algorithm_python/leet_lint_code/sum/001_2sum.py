12b8d686aa9227633d0efe145052906dd886d008
evenhumble <patrickwuke@163.com>
2019-08-04 14:44:06 +0800

    add 4 solution

--- algorithm_python/leet_lint_code/sum/001_2sum.py
+++ algorithm_python/leet_lint_code/sum/001_2sum.py
@@ -11,6 +11,13 @@ Example:
 Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
+
+Solution:
+首先 a+b =target
+1. 定义一个dict，保存内容为 循环当前数和目标值的差值以及当前值的索引位置
+2. 循环nums，如果当前值在定义的dict中，那么返回当前值的索引和当前值在dict中对应的值
+否则就保存循环当前数和目标值的差值以及当前值的索引位置
+3. 遍历所有的值如果没有找到结果，则返回空list
 ```
 """
 
@@ -49,3 +56,23 @@ class Solution_FASTER(object):
             temp[target - d] = i
 
         return []
+
+
+class Solution_EasyUnderstand:
+    def twoSum(self, nums, target):
+        if len(nums) < 2:
+            return []
+        temp = {}  ## key: the left value, value: the first index
+        # for idx, value in enumerate(nums):
+        #     temp[value] = idx
+        for idx, value in enumerate(nums):
+            if value in temp:
+                return [temp[value], idx]
+            else:
+                temp[target - value] = idx
+        return []
+
+
+if __name__ == '__main__':
+    result = Solution_EasyUnderstand().twoSum([3, 2, 4], 6)
+    print(result)
